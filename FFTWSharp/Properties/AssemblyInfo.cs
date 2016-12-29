4765171a5eedc4b9fca42eced64f14007981b51a
Andras Fuchs <andras.fuchs@gmail.com>
2016-12-29 12:16:40 +0700

    Fixed a type in AssemblyInfo
    Fixed a type in AssemblyInfo

--- FFTWSharp/Properties/AssemblyInfo.cs
+++ FFTWSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FFTW Library .NET wrapper")]
-[assembly: AssemblyDescription(".NET wrapper class for .NET")]
+[assembly: AssemblyDescription("FFTW .NET wrapper class")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tamas Szalay")]
 [assembly: AssemblyProduct("FFTW Library .NET wrapper")]
